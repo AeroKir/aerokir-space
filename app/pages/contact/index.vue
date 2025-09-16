6aeaa159b39d3b027153af648cf519565a0a584d
Kirill Shevtsov <kirwork.em@gmail.com>
2025-09-17 00:43:02 +0300

    contact page styles&data via useAppConfig

--- app/pages/contact/index.vue
+++ app/pages/contact/index.vue
@@ -1,24 +1,45 @@
 <template>
   <Page>
     <AppHeader />
-    <main class="h-screen w-[652px] ml-[407px] text-primaryDark dark:text-accentColor relative">
+    <main class="h-auto w-[652px] ml-[407px] text-primaryDark dark:text-accentColor relative">
       <IconSmile
-        class="w-[243px] h-[243px] absolute top-8 right-8 text-primaryDarkAlpha25 dark:text-primaryLightAlpha50"
+        class="w-[243px] h-[243px] absolute -top-[70px] right-32 text-primaryDarkAlpha25 dark:text-primaryLightAlpha50"
       />
       <HeadingPage
         :title="$t('pages.contact.title')"
         :isDot="true"
       />
-      <p>Are you interested in me as a person, or do you want to do a project? Let's stay in touch.</p>
+      <p class="font-body font-bold text-xl text-primaryDark dark:text-primaryLight mb-10">
+        Are you interested in me as a person, or do you want to do a project? Let's stay in touch.
+      </p>
       <ul>
-        <li>Email: <a href="mailto:shev.kv.1982@gmail.com">shev.kv.1982@gmail.com</a></li>
-        <!-- <li>
+        <li class="font-heading mb-1">
+          Email: <a href="mailto:shev.kv.1982@gmail.com">shev.kv.1982@gmail.com</a>
+        </li>
+        <li class="font-heading mb-1">
           Telegram: <a
-            href="https://t.me/aerokir"
+            :href="socials.telegram"
             target="_blank"
-            rel="noopener"
-          >@aerokir</a>
-        </li> -->
+            rel="noopener noreferrer"
+          >@Kirill_Shevtsov</a>
+        </li>
+        <li class="font-heading mb-1">
+          Phone: <a :href="`tel:${profile.phone}`">{{ profile.phone }}</a>
+        </li>
+        <li class="font-heading mb-1">
+          GitHub: <a
+            :href="socials.github"
+            target="_blank"
+            rel="noopener noreferrer"
+          >github.com/AeroKir</a>
+        </li>
+        <li class="font-heading">
+          Linkedin: <a
+            :href="socials.linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >linkedin.com/in/aerokir-space/</a>
+        </li>
       </ul>
     </main>
     <AppFooter />
@@ -27,4 +48,17 @@
 
 <script setup lang="ts">
 import IconSmile from '../../assets/icons/smile.svg';
+
+interface Profile {
+  phone: string;
+  email: string;
+}
+
+interface Socials {
+  telegram: string;
+  github: string;
+  linkedin: string;
+}
+
+const { profile, socials } = useAppConfig() as unknown as { profile: Profile; socials: Socials };
 </script>
