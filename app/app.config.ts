5210b97f68e8abe82ba99d20e5b8f6b241710aae
Kirill Shevtsov <kirwork.em@gmail.com>
2025-09-17 00:41:51 +0300

    app.config created

---
+++ app/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  profile: {
+    name: 'Kirill Shevtsov',
+    position: 'Frontend Developer',
+    email: 'shev.kv.1982@gmail.com',
+    phone: '+380684479024',
+    city: 'Kharkiv, Ukraine',
+    // photo: '/photo.jpg',
+  },
+  socials: {
+    github: 'https://github.com/AeroKir',
+    telegram: 'https://t.me/Kirill_Shevtsov',
+    linkedin: 'https://www.linkedin.com/in/aerokir-space/' },
+});
