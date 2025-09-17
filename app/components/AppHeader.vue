2f098fc77a9b6d407880c23cfc9777e23f482e86
Kirill Shevtsov <kirwork.em@gmail.com>
2025-09-17 22:55:57 +0300

    AppHeader: fixed position

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-end items-start gap-4 px-20 pt-20 pb-12">
+  <header class="fixed top-0 left-0 right-0 z-50 flex justify-end items-start gap-4 px-20 pt-20 pb-12">
     <AppMenu />
     <div class="flex items-center">
       <ThemeSwitcher />
