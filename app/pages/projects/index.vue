c5062ed222d4d4d9c21535d55f0113dc5df01be3
Kirill Shevtsov <kirwork.em@gmail.com>
2025-09-23 02:01:52 +0300

    projects page: base rendering

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -1,7 +1,7 @@
 <template>
   <Page>
     <AppHeader />
-    <main class="h-screen w-screen ml-[283px] text-primaryDark dark:text-white relative">
+    <main class="h-full w-screen ml-[283px] text-primaryDark dark:text-white relative">
       <HeadingPage
         :title="$t('pages.projects.title')"
         :isDot="true"
@@ -9,6 +9,35 @@
       <IconPyramidVertical
         class="w-[343px] h-[343px] absolute -top-[155px] right-1/2 text-primaryDarkAlpha25 dark:text-primaryLightAlpha50"
       />
+
+      <ul class="grid gap-6">
+        <li
+          v-for="project in projects"
+          :key="project.id"
+          class="w-[400px] p-4 border rounded"
+        >
+          <!-- <img
+            :src="project.image"
+            alt=""
+            class="w-full h-40 object-cover mb-2"
+          > -->
+          <h3 class="text-xl font-heading">
+            {{ $t(project.name) }}
+          </h3>
+          <p class="text-sm">
+            {{ $t(project.description) }}
+          </p>
+          <a
+            :href="project.link"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-accentColor underline"
+          >
+            <!-- {{ $t('projects.viewMore') }} -->
+            Link
+          </a>
+        </li>
+      </ul>
     </main>
     <AppFooter />
   </Page>
@@ -16,6 +45,17 @@
 
 <script setup lang="ts">
 import IconPyramidVertical from '../../assets/icons/pyramid-vertical.svg';
+
+const { projects } = useAppConfig() as {
+  projects: {
+    id: number | string;
+    name: string;
+    description: string;
+    link: string;
+    image: string;
+    // tags: string[];
+  }[];
+};
 </script>
 
 <style scoped>
