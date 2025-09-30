b25e470bd81c85b898b7bd66142a2ddaba151bbe
Kirill Shevtsov <kirwork.em@gmail.com>
2025-10-01 00:11:00 +0300

    Projects page styles&AppHeader z-index set to 40

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -1,15 +1,17 @@
 <template>
   <Page>
     <AppHeader />
-    <main class="h-full ml-[283px] mr-10 text-primaryDark dark:text-white relative">
-      <HeadingPage
-        :title="$t('pages.projects.title')"
-        :isDot="true"
-        customClass="ml-[407px] mb-[100px]"
-      />
-      <IconPyramidVertical
-        class="w-[343px] h-[343px] absolute -top-[155px] right-1/2 text-primaryDarkAlpha25 dark:text-primaryLightAlpha50"
-      />
+    <main class="h-full ml-[283px] mr-[265px] -top-20 text-primaryDark dark:text-white relative z-50">
+      <div class="relative max-w-[1040px] h-auto mx-auto -translate-x-1/2 pb-[70px]">
+        <HeadingPage
+          :title="$t('pages.projects.title')"
+          :isDot="true"
+          customClass="ml-[500px] mb-[100px]"
+        />
+        <IconPyramidVertical
+          class="w-[343px] h-[343px] absolute top-[23px] -right-[400px] text-primaryDarkAlpha25 dark:text-primaryLightAlpha50"
+        />
+      </div>
 
       <section>
         <ProjectPreviewCard
@@ -26,29 +28,6 @@
           :previewVideo="project.previewVideo"
         />
       </section>
-
-      <!-- <ul class="grid gap-6">
-        <li
-          v-for="project in projects"
-          :key="project.id"
-          class="w-[400px] p-4 border rounded"
-        >
-          <h3 class="text-xl font-heading">
-            {{ $t(project.name) }}
-          </h3>
-          <p class="text-sm">
-            {{ $t(project.description) }}
-          </p>
-          <a
-            :href="project.link"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="text-accentColor underline"
-          >
-            Link
-          </a>
-        </li>
-      </ul> -->
     </main>
     <AppFooter />
   </Page>
@@ -70,7 +49,6 @@ const { projects } = useAppConfig() as {
     yearsOfDevelopment: string;
     projectType: string;
     previewVideo?: string;
-    // tags: string[];
   }[];
 };
 </script>
