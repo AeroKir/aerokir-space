cc286ea620d3bd29a95fbd0bf36b73e1062228b2
Kirill Shevtsov <kirwork.em@gmail.com>
2025-10-03 22:23:59 +0300

    projects slug page: base markup&styling

--- app/pages/projects/[slug].vue
+++ app/pages/projects/[slug].vue
@@ -1,7 +1,11 @@
 <template>
   <Page>
     <AppHeader />
-    <main class="max-w-3xl mx-auto py-16 px-6">
+    <!-- <PerspectiveGrid
+      position="top"
+      :height="490"
+    /> -->
+    <main class="max-w-3xl mx-auto px-6">
       <NuxtLink
         :to="localePath('/projects')"
         class="text-accentColor underline mb-6 inline-block"
@@ -17,42 +21,54 @@
           v-if="project"
           :key="project.slug"
         >
-          <h1 class="text-4xl font-heading mb-4">
+          <h1 class="text-[128px] font-heading font-bold text-primaryDark dark:text-primaryLight">
             {{ t(project.name) }}
           </h1>
-          <p class="text-lg text-primaryDark dark:text-primaryLight mb-6">
-            {{ t(project.description) }}
-          </p>
-
-          <img
-            :src="project.image"
-            :alt="project.name"
-            class="w-full rounded-lg shadow mb-8"
-          >
-
-          <p class="mb-4">
-            <strong>Years:</strong> {{ project.yearsOfDevelopment }}
-          </p>
-          <p class="mb-4">
-            <strong>Type:</strong> {{ project.projectType }}
-          </p>
-          <p class="mb-4">
-            <strong>Technologies:</strong> {{ project.technologies.join(', ') }}
-          </p>
-
-          <div class="flex gap-4 mt-6">
-            <a
-              v-if="project.link"
-              :href="project.link"
-              target="_blank"
-              class="text-accentColor underline"
-            >Visit site</a>
+          <div class="flex gap-4 justify-between mt-6 border-t border-b border-primaryDark dark:border-primaryLight py-4 mb-6">
+            <p class="">
+              <strong>Type:</strong> {{ t(project.projectType) }}
+            </p>
+            <p class="">
+              <strong>Years:</strong> {{ project.yearsOfDevelopment }}
+            </p>
             <a
               v-if="project.linkGitHub"
               :href="project.linkGitHub"
               target="_blank"
               class="text-accentColor underline"
             >View on GitHub</a>
+            <a
+              v-if="project.link"
+              :href="project.link"
+              target="_blank"
+              class="text-accentColor underline"
+            >Visit site</a>
+          </div>
+          <p class="text-lg text-primaryDark dark:text-primaryLight mb-6">
+            {{ t(project.description) }}
+          </p>
+          <p class="mb-4">
+            <strong>Techstack:</strong> {{ project.technologies.join(', ') }}
+          </p>
+          <img
+            :src="project.image"
+            :alt="project.name"
+            class="w-full rounded-lg shadow mb-8"
+          >
+          <div
+            class="mb-4"
+          >
+            <h2 class="text-2xl font-heading font-bold text-primaryDark dark:text-primaryLight mb-2">
+              Solved Tasks and Results
+            </h2>
+            <ul class="text-lg text-primaryDark dark:text-primaryLight mb-2">
+              <li
+                v-for="task in project.solvedTasks"
+                :key="task"
+              >
+                • {{ task }}
+              </li>
+            </ul>
           </div>
         </article>
       </transition>
