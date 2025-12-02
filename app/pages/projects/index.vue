9c0ea494aaaf394c898a90cdaea956d06d2ecdfa
Kirill Shevtsov <kirwork.em@gmail.com>
2025-12-02 18:49:41 +0200

    projects index page: 2xl&3xl breakpoints styling

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -6,7 +6,7 @@
         <HeadingPage
           :title="$t('pages.projects.title')"
           :isDot="true"
-          customClass="ml-[0] md:-ml-[10px] xl:ml-16 2xl:ml-0 mb-4 md:mb-[100px] pt-6 md:mt-12 lg:mt-0 lg:pt-0"
+          customClass="ml-[0] md:-ml-[10px] xl:ml-16 3xl:ml-0 mb-4 md:mb-[100px] pt-6 md:mt-12 lg:mt-0 lg:pt-0"
         >
           <IconPyramidVertical
             class="w-[150px] h-[150px] md:w-[200px] md:h-[200px] lg:w-[343px] lg:h-[343px] xl:w-[250px] xl:h-[250px] 2xl:w-[343px] 2xl:h-[343px] absolute top-1 md:top-6 lg:-top-12 xl:-top-1 2xl:top-[23px] -right-[80px] md:-right-[105px] lg:-right-[185px] xl:-right-[120px] 2xl:-right-[185px] text-primaryDarkAlpha25 dark:text-primaryLightAlpha50 z-0"
