bb37b496179c33cac2e6bcf057aa9d0ddd7f584a
Kirill Shevtsov <kirwork.em@gmail.com>
2026-01-06 00:23:58 +0200

    main page: hero section

---
+++ app/components/AvatarImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-[600px] h-[700px] flex justify-center items-center bg-primaryLight -rotate-[5deg]">
+    <img
+      src="/avatar-image.png"
+      alt=""
+      class="w-[580px] h-[680px] object-cover"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,95 +1,51 @@
 <template>
-  <div class="h-screen w-screen bg-sepia dark:bg-primaryDark text-primaryDark dark:text-white relative overflow-x-hidden pt-[312px]">
+  <Page>
     <AppHeader />
-    <!-- <NuxtLink
-      to="https://aerokir.github.io/portfolio/"
-      external
-    >Prev portfolio</NuxtLink> -->
-
-    <!-- <div class="relative w-full h-screen overflow-hidden">
-      <div class="perspective-grid" />
-    </div> -->
-    <!-- <PerspectiveGrid /> -->
     <PerspectiveGrid
       position="top"
       :height="490"
     />
-    <h1
-      class="centered text-primaryDark dark:text-accentColor font-heading font-bold text-[200px]"
-    >
-      {{ runtimeConfig.public.helloText }}
-    </h1>
-    <!-- <p class="h-[500px] bg-sepia dark:bg-primaryDark text-primaryDark dark:text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt obcaecati minus magnam autem fugit natus rem ut rerum quasi repellendus distinctio voluptatum dolores quisquam dignissimos, praesentium veritatis, mollitia beatae! Saepe.</p> -->
-    <p
-      class="w-1/2 m-auto text-center"
-    >
-      {{ t('introduce.para1') }}
-      {{ t('introduce.para2') }}
-    </p>
-    <!-- <PerspectiveGrid
-      position="bottom"
-      :height="200"
-      customClass="bg-sepia dark:bg-primaryDark"
-    /> -->
+    <main class="h-screen mx-auto xl:ml-[230px] xl:mr-[230px] 2xl:ml-[283px] 2xl:mr-[265px] md:-top-[520px] text-primaryDark dark:text-primaryLight relative xl:z-0 px-2 md:px-8 lg:px-20 xl:px-0">
+      <HeadingPage
+        :title="runtimeConfig.public.helloText"
+        customClass="ml-[0] md:-ml-[10px] xl:ml-[60px] 3xl:ml-0 mb-12 md:mb-[150px] pt-6 md:mt-12 lg:mt-0 lg:pt-0"
+      />
+      <!-- <h1
+        class="centered text-primaryDark dark:text-accentColor font-heading font-bold text-[200px]"
+      >
+        {{ runtimeConfig.public.helloText }}
+      </h1> -->
+      <div class="relative">
+        <div class="relative w-fit">
+          <IconTriangles class="absolute z-10 -top-9 -right-12 w-[120px] h-[120px] text-primaryDark dark:text-accentColor" />
+          <AvatarImage />
+          <IconPyramid class="absolute z-10 -bottom-70 right-1/2 translate-x-1/2 w-[250px] h-[250px] text-primaryDark dark:text-primaryLight" />
+        </div>
+
+        <p
+          class="relative max-w-[780px] ml-auto -mt-40 font-body text-lg md:text-xl lg:text-2xl font-normal leading-relaxed [text-shadow:0_1px_1px_#9DA3A4] dark:[text-shadow:0_2px_3px_#000000]"
+        >
+          {{ t('introduce.para1') }}
+          {{ t('introduce.para2') }}
+        </p>
+      </div>
+    </main>
+
     <SkillsSection />
-    <AppFooter />
-  </div>
+    <AppFooter>
+      <AppFooterInternalLink
+        :to="'/experience'"
+        :label="$t('footerLinks.toExperience')"
+      />
+    </AppFooter>
+  </Page>
 </template>
 
 <script setup lang="ts">
-// import AppHeader from '~/components/AppHeader.vue';
-// import PerspectiveGrid from '~/components/PerspectiveGrid.vue';
+import IconTriangles from '~/assets/icons/triangles.svg';
+import IconPyramid from '~/assets/icons/pyramid-3d.svg';
 
 const runtimeConfig = useRuntimeConfig();
-const colors = ['#f87171', '#fb923c', '#fbbf24', '#facc15', '#a3e635', '#4ade80', '#34d399', '#2dd4bf', '#22d3ee', '#38bdf8', '#60a5fa', '#818cf8', '#a78bfa', '#c084fc', '#e879f9', '#f472b6', '#fb7185'];
-const color = useState('color', () => colors[Math.floor(Math.random() * colors.length)]);
+
 const { t } = useI18n();
 </script>
-
-<style scoped>
-.centered {
-  position: absolute;
-  width: 50%;
-  text-align: center;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  margin: 0;
-  /* font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; */
-}
-h1 {
-  font-size: 32px;
-}
-@media (min-width: 768px) {
-  h1 {
-    font-size: 64px;
-  }
-}
-a {
-  color: #888;
-  text-decoration: none;
-  font-size: 18px;
-}
-a:hover {
-  text-decoration: underline;
-}
-
-.perspective-grid {
-  position: absolute;
-  inset: 0;
-  background:
-    repeating-linear-gradient(to right, rgba(0,0,0,0.5) 0 1px, transparent 1px 100px),
-    repeating-linear-gradient(to bottom, rgba(0,0,0,0.5) 0 1px, transparent 1px 100px);
-  transform: perspective(600px) rotateX(60deg);
-  animation: moveGrid 3s linear infinite;
-}
-
-@keyframes moveGrid {
-  from {
-    background-position: 0 0, 0 0;
-  }
-  to {
-    background-position: 0 100px, 0 100px;
-  }
-}
-</style>
